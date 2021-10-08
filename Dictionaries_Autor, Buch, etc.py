2ee5eeba219d86f74b0119a2b6fb043c34560f5f
krisayo <58567572+krisayo@users.noreply.github.com>
2021-10-08 11:48:03 +0200

    Add files via upload

---
+++ Dictionaries_Autor, Buch, etc.py
@@ -0,0 +1,18 @@
+Buch1 = {"author":"Kazuo Ishiguro", "title":"Klara and the Sun", "number":723429, "price":18.99}
+Buch2 = {"author":"Jack London", "title":"Martin Eden", "number":722339, "price":13.99}
+Buch3 = {"author":"Virginia Woolf", "title":"Orlando", "number":712429, "price":11.99}
+
+print (Buch1["author"])
+print (Buch1["title"])
+print (Buch1["number"])
+print (Buch1["price"], "\n")
+
+print (Buch2["author"])
+print (Buch2["title"])
+print (Buch2["number"])
+print (Buch2["price"], "\n")
+
+print (Buch3["author"])
+print (Buch3["title"])
+print (Buch3["number"])
+print (Buch3["price"], "\n")
---
+++ Liste_Autor, Buch, etc.py
@@ -0,0 +1,18 @@
+Buch1 = ["Kazuo Ishiguro", "Klara and the Sun", 782138, 18.99]
+Buch2 = ["Jack London", "Martin Eden", 832173, 8.95]
+Buch3 = ["Virginia Woolf", "Orlando", 738123, 11.99]
+
+print (Buch1[0])
+print (Buch1[1])
+print (Buch1[2])
+print (Buch1[3], "\n")
+
+print (Buch2[0])
+print (Buch2[1])
+print (Buch2[2])
+print (Buch2[3], "\n")
+
+print (Buch3[0])
+print (Buch3[1])
+print (Buch3[2])
+print (Buch3[3], "\n")
